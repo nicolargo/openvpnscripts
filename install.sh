f887b89f0be039303fd9b0cf89b68183ed9ff7f6
Kimpe Andy <andykimpe@gmail.com>
2013-03-02 10:03:02 +0100

    Update install.sh
    Update install.sh

--- install.sh
+++ install.sh
@@ -69,39 +69,101 @@ then
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y install openvpn sudo zip unzip
-mkdir /etc/openvpn/
-mkdir /etc/openvpn/easy-rsa/
-cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0/* /etc/openvpn/easy-rsa/
-cp /etc/openvpn/easy-rsa/openssl-1.0.0.cnf /etc/openvpn/easy-rsa/openssl.cnf
+cd /etc/openvpn
+git clone git://github.com/OpenVPN/easy-rsa.git /etc/openvpn/test
+mkdir /etc/openvpn/easy-rsa
+cp -R /etc/openvpn/test/easy-rsa/2.0/* /etc/openvpn/easy-rsa
+rm -rf /etc/openvpn/test
 chown -R $USER /etc/openvpn/easy-rsa/
-
-rm /etc/openvpn/easy-rsa/vars
+cd /etc/openvpn/easy-rsa/
+chmod 755 *
+rm -f /etc/openvpn/easy-rsa/vars
+touch /etc/openvpn/easy-rsa/vars
 cat > /etc/openvpn/easy-rsa/vars <<EOF
+# easy-rsa parameter settings
+
+# NOTE: If you installed from an RPM,
+# don't edit this file in place in
+# /usr/share/openvpn/easy-rsa --
+# instead, you should copy the whole
+# easy-rsa directory to another location
+# (such as /etc/openvpn) so that your
+# edits will not be wiped out by a future
+# OpenVPN package upgrade.
+
+# This variable should point to
+# the top level of the easy-rsa
+# tree.
 export EASY_RSA="/etc/openvpn/easy-rsa/"
+
+#
+# This variable should point to
+# the requested executables
+#
 export OPENSSL="openssl"
 export PKCS11TOOL="pkcs11-tool"
 export GREP="grep"
-export KEY_CONFIG=/etc/openvpn/easy-rsa/openssl.cnf
+
+
+# This variable should point to
+# the openssl.cnf file included
+# with easy-rsa.
+export KEY_CONFIG=/etc/openvpn/easy-rsa/openssl-1.0.0.cnf
+
+# Edit this variable to point to
+# your soon-to-be-created key
+# directory.
+#
+# WARNING: clean-all will do
+# a rm -rf on this directory
+# so make sure you define
+# it correctly!
 export KEY_DIR="/etc/openvpn/easy-rsa/keys"
+
+# Issue rm -rf warning
 echo NOTE: If you run ./clean-all, I will be doing a rm -rf on $KEY_DIR
+
+# PKCS11 fixes
 export PKCS11_MODULE_PATH="dummy"
 export PKCS11_PIN="dummy"
+
+# Increase this to 2048 if you
+# are paranoid.  This will slow
+# down TLS negotiation performance
+# as well as the one-time DH parms
+# generation process.
 export KEY_SIZE=1024
+
+# In how many days should the root CA key expire?
 export CA_EXPIRE=3650
+
+# In how many days should certificates expire?
 export KEY_EXPIRE=3650
+
+# These are the default values for fields
+# which will be placed in the certificate.
+# Don't leave any of these fields blank.
 export KEY_COUNTRY="$country"
 export KEY_PROVINCE="$dep"
 export KEY_CITY="$ville"
 export KEY_ORG="$org"
 export KEY_EMAIL="$email"
+export KEY_EMAIL=$email
+export KEY_CN=changeme
+export KEY_NAME=changeme
+export KEY_OU=changeme
+export PKCS11_MODULE_PATH=changeme
+export PKCS11_PIN=1234
 EOF
-cd /etc/openvpn/easy-rsa/
-source vars
+
+mkdir keys
+chmod 755 *
+source ./vars
 ./vars
 ./clean-all
+./build-ca
+./build-key-server server
 ./build-dh
-./pkitool --initca
-./pkitool --server server
 openvpn --genkey --secret keys/ta.key
 cp keys/ca.crt keys/ta.key keys/server.crt keys/server.key keys/dh1024.pem /etc/openvpn/
 mkdir /etc/openvpn/chroot
@@ -195,7 +257,6 @@ then
 rpm -Uvh lzo-*.rpm
 rm lzo-*.rpm
 yum install openvpn -y
-cp -R /usr/share/doc/openvpn-2.2.2/easy-rsa/ /etc/openvpn/
 cd /etc/openvpn
 git clone git://github.com/OpenVPN/easy-rsa.git /etc/openvpn/test
 mkdir /etc/openvpn/easy-rsa
