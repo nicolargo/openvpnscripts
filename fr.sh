fb4af7a067182758f17c80fa8c75bdbf76fe1c5f
Kimpe Andy <andykimpe@gmail.com>
2013-12-21 03:30:19 +0100

    Update fr.sh

--- fr.sh
+++ fr.sh
@@ -2,3 +2,4 @@
 country="Entrez les premierre lettre de votre pays en majuscule"
 country2="ex : pour la france FR"
 country3="Entrez les premierre lettre de votre pays en majuscule  : "
+dep="Entrez le numero de votre departemant :  "
