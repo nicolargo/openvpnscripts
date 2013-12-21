bf9ff3c5a8217e115fc401e4fcbb68e1b38296c4
Kimpe Andy <andykimpe@gmail.com>
2013-12-21 03:31:03 +0100

    Update en.sh

--- en.sh
+++ en.sh
@@ -2,3 +2,4 @@
 country="Enter the first letter of your country in uppercase"
 country2="eg : for France FR"
 country3="Enter the first letter of your country in uppercase  : "
+dep="Enter the number of your department : "
