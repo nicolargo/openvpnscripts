8aaf3af27a019eb8afdcfc3e14435f562b200f31
Kimpe Andy <andykimpe@gmail.com>
2013-12-21 03:33:50 +0100

    Update en.sh

--- en.sh
+++ en.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-country="Enter the first letter of your country in uppercase"
+country1="Enter the first letter of your country in uppercase"
 country2="eg : for France FR"
 country3="Enter the first letter of your country in uppercase  : "
-dep="Enter the number of your department : "
+dep1="Enter the number of your department : "
